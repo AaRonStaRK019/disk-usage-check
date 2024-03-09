5716b6050d5c00428976c02ebbe6ec0cce6ce568
Emmanuel Nwokeocha <61948248+AaRonStaRK019@users.noreply.github.com>
2024-03-09 09:01:37 +0100

    commented on steps

--- backup.sh
+++ backup.sh
@@ -2,6 +2,7 @@
 
 # a script to backupa chosen directory as a tar archive
 
+# check if two arguments are provided
 if [ "$#" -ne 2 ]; then
 	echo "usage: $0 <source_directory> <destination_directory>"
 	exit 1
@@ -10,22 +11,19 @@ fi
 src="$1"
 destination="$2"
 
+#check if source is a valid directory
 if [ ! -d "$src" ]; then
 	echo "Error $src is not a valid directory."
 	exit 1
 fi
 
-
+#create the destination directory if it doesn't exist
 mkdir -p "$destination"
 
+#create timestamp
 timestamp=$(date +"%Y%m%d%H%M%S")
 
-#tar -czf "$destination/backup_$timestamp.tar.gz" -C "$(dirname "$src")" "$(basename "$src")"
-
-#echo "backup successful. Archive saved to $destination/backup_$timestamp.tar"
-
-
-# Compress the source directory as a tar archive and move to the destination directory
+# create tar archive and move to the destination directory
 tar -cf "$destination/backup_$timestamp.tar" -C "$(dirname "$src")" "$(basename "$src")"
 
 echo "Backup completed successfully. Archive saved to $destination/backup_$timestamp.tar"
